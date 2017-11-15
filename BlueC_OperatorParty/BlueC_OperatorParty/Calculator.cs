731d913b436fb63fb8739ab76801f246ed37dc69
jesper0438 <robr0002@hz.nl>
2017-11-15 11:37:17 +0100

    rekenmachine

---
+++ BlueC_OperatorParty/BlueC_OperatorParty/Calculator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BlueC_OperatorParty
+{
+    class Calculator
+    {
+        public int Min(int getal1, int getal2)
+        {
+            return getal1 - getal2;
+        }
+        public int Plus(int getal1, int getal2)
+        {
+            return getal1 + getal2;
+        }
+        public int Keer(int getal1, int getal2)
+        {
+            return getal1 * getal2;
+        }
+        public int Gedeeld(int getal1, int getal2)
+        {
+            return getal1 / getal2;
+        }
+    }
+}
--- BlueC_OperatorParty/BlueC_OperatorParty/Program.cs
+++ BlueC_OperatorParty/BlueC_OperatorParty/Program.cs
@@ -12,19 +12,54 @@ namespace BlueC_OperatorParty
 
             string[] opgesplitsteInvoer = invoer.Split(" ");
 
-            string operatorr = opgesplitsteInvoer[0];
+            string operatorr = opgesplitsteInvoer[1];
 
-            string getal1 = opgesplitsteInvoer[1];
-            int number1 = int.Parse(getal1);
+            string number1 = opgesplitsteInvoer[0];
+            int getal1 = int.Parse(number1);
 
-            string getal2 = opgesplitsteInvoer[2];
-            int number2 = int.Parse(getal2);
+            string number2 = opgesplitsteInvoer[2];
+            int getal2 = int.Parse(number2);
 
             if (operatorr == "+")
             {
+                var programma = new Calculator();
+
+                int result = programma.Plus(getal1, getal2);
+
+                Console.WriteLine(result);
+                Console.ReadLine();
                 // roep hier netjes de methode
                 // van de calculatorclass aan
-                int r = number1 + number2;
+                
+            }
+            if (operatorr == "*")
+            {
+                var programma = new Calculator();
+
+                int result = programma.Keer(getal1, getal2);
+
+                Console.WriteLine(result);
+                Console.ReadLine();
+            }
+            
+            if (operatorr == "-")
+            {
+                var programma = new Calculator();
+
+                int result = programma.Min(getal1, getal2);
+
+                Console.WriteLine(result);
+                Console.ReadLine();
+            }
+
+            if (operatorr == "/")
+            {
+                var programma = new Calculator();
+
+                int result = programma.Gedeeld(getal1, getal2);
+
+                Console.WriteLine(result);
+                Console.ReadLine();
             }
         }
     }
