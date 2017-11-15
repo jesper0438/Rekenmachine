5b2cfe5e026e3cc8003d2ca420c462bddabdeba7
jesper0438 <robr0002@hz.nl>
2017-11-15 13:23:21 +0100

    while loopie

--- BlueC_OperatorParty/BlueC_OperatorParty/Program.cs
+++ BlueC_OperatorParty/BlueC_OperatorParty/Program.cs
@@ -6,60 +6,64 @@ namespace BlueC_OperatorParty
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Geef een som in (prefix notatie)...");
-
-            string invoer = Console.ReadLine();
-
-            string[] opgesplitsteInvoer = invoer.Split(" ");
-
-            string operatorr = opgesplitsteInvoer[1];
-
-            string number1 = opgesplitsteInvoer[0];
-            int getal1 = int.Parse(number1);
-
-            string number2 = opgesplitsteInvoer[2];
-            int getal2 = int.Parse(number2);
-
-            if (operatorr == "+")
+            while (true)
             {
-                var programma = new Calculator();
 
-                int result = programma.Plus(getal1, getal2);
+                Console.WriteLine("Geef een som in (prefix notatie)...");
 
-                Console.WriteLine(result);
-                Console.ReadLine();
-                // roep hier netjes de methode
-                // van de calculatorclass aan
-                
-            }
-            if (operatorr == "*")
-            {
-                var programma = new Calculator();
+                string invoer = Console.ReadLine();
 
-                int result = programma.Keer(getal1, getal2);
+                string[] opgesplitsteInvoer = invoer.Split(" ");
 
-                Console.WriteLine(result);
-                Console.ReadLine();
-            }
-            
-            if (operatorr == "-")
-            {
-                var programma = new Calculator();
+                string operatorr = opgesplitsteInvoer[1];
 
-                int result = programma.Min(getal1, getal2);
+                string number1 = opgesplitsteInvoer[0];
+                int getal1 = int.Parse(number1);
 
-                Console.WriteLine(result);
-                Console.ReadLine();
-            }
+                string number2 = opgesplitsteInvoer[2];
+                int getal2 = int.Parse(number2);
 
-            if (operatorr == "/")
-            {
-                var programma = new Calculator();
+                if (operatorr == "+")
+                {
+                    var programma = new Calculator();
 
-                int result = programma.Gedeeld(getal1, getal2);
+                    int result = programma.Plus(getal1, getal2);
 
-                Console.WriteLine(result);
-                Console.ReadLine();
+                    Console.WriteLine(result);
+                    Console.ReadLine();
+                    // roep hier netjes de methode
+                    // van de calculatorclass aan
+
+                }
+                if (operatorr == "*")
+                {
+                    var programma = new Calculator();
+
+                    int result = programma.Keer(getal1, getal2);
+
+                    Console.WriteLine(result);
+                    Console.ReadLine();
+                }
+
+                if (operatorr == "-")
+                {
+                    var programma = new Calculator();
+
+                    int result = programma.Min(getal1, getal2);
+
+                    Console.WriteLine(result);
+                    Console.ReadLine();
+                }
+
+                if (operatorr == "/")
+                {
+                    var programma = new Calculator();
+
+                    int result = programma.Gedeeld(getal1, getal2);
+
+                    Console.WriteLine(result);
+                    Console.ReadLine();
+                }
             }
         }
     }
